73cbf1832e553839d58ac22defd1c356182637fb
praveenise74 <praveenise74@gmail.com>
2017-04-30 22:36:58 +0530

    Updaing book_status file

--- book_status.java
+++ book_status.java
@@ -15,12 +15,10 @@ import com.mysql.jdbc.ResultSet;
 public class book_status extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+   
     public book_status() {
         super();
-        // TODO Auto-generated constructor stub
+     
     }
 
 	
@@ -29,11 +27,9 @@ public class book_status extends HttpServlet {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		doGet(request, response);
 		
 		long pnr =Long.parseLong(request.getParameter("pnr"));
@@ -57,11 +53,11 @@ public class book_status extends HttpServlet {
 			 request.setAttribute("status1", "false");
 		 }
 		 
-		 //RequestDispatcher dispatcher=request.getRequestDispatcher("main");
-		 System.out.println("it worked here1");
+		
+		 System.out.println(" code running status checking point 1 ");
 		 System.out.println(bs.dbpnr);
 	
-		// System.out.println(destination);
+		 System.out.println("Code running status Destination ="+destination);
 		 request.getRequestDispatcher("main.jsp").forward(request, response);
 		
 		
